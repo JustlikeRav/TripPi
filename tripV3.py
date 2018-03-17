3352ccb7f4aba74b96d73126abb77c65b1904420
Ravneet Singh <30868824+JustlikeRav@users.noreply.github.com>
2018-03-17 19:14:41 +0000

    Update tripV3.py

--- tripV3.py
+++ tripV3.py
@@ -11,7 +11,10 @@ device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
 def print_data(lat,lng,temp):
-	print "Latitude: " + lat + " | Longitude: " + lon + " | Temperature: " + temp)
+    print "Latitude: " + lat + " | Longitude: " + lng + " | Temperature: " + temp
+    
+    #Save To Database
+    
  
 def read_temp_raw():
     f = open(device_file, 'r')
@@ -51,8 +54,8 @@ while x == 0:
        if fix > 1:
           lat = " " + gps[18:20] + "." + gps[20:22] + "." + gps[23:27] + gps[28:29]
           lon = " " + gps[30:33] + "." + gps[33:35] + "." + gps[36:40] + gps[41:42]
-		  print_data(lat,lng,str(read_temp())
+	  print_data(lat,lon,str(read_temp()))
        else:
           lat = "No Valid Data"
           lon = "No Valid Data"
-		  print_data(lat,lng,str(read_temp())
+	  print_data(lat,lon,str(read_temp()))
