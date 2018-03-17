0d29ef41cdb25b84ebfb1f544706672b6680c2f4
DESKTOP-8A2NBQ9\Rav <ravneetsngh6@gmail.com>
2018-03-17 15:33:19 -0400

    Sending dummy data
    Sending dummy data to the server php file.

--- tripV3.py
+++ tripV3.py
@@ -14,7 +14,13 @@ def print_data(lat,lng,temp):
     print "Latitude: " + lat + " | Longitude: " + lng + " | Temperature: " + temp
     
     #Save To Database
-    
+    mydata=[('one','1'),('two','2')]    #The first is the var name the second is the value
+	mydata=urllib.urlencode(mydata)
+	path='http://www.justlikerav.com/trippie/data.php'    #the url you want to POST to
+	req=urllib2.Request(path, mydata)
+	req.add_header("Content-type", "application/x-www-form-urlencoded")
+	page=urllib2.urlopen(req).read()
+	print page
  
 def read_temp_raw():
     f = open(device_file, 'r')
