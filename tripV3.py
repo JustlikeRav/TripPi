8e0e013676c9159c78e627d0ba1235282737ffd2
Ravneet Singh <30868824+JustlikeRav@users.noreply.github.com>
2018-03-18 20:01:50 +0000

    indent fixed

--- tripV3.py
+++ tripV3.py
@@ -53,20 +53,18 @@ fix = 1
 
 x = 0
 while x == 0:
-   gps = ser.readline()
+    gps = ser.readline()
 
-	if gps[1:6] == "GPGSA":
-      fix = int(gps[9:10])
-	if gps[1 : 6] == "GPGGA":
-	lambda lat,lon
-	temp = str(read_temp())
-       if fix > 1:
-          lat = str(CoordinateToDouble(float(gps[18:20]), float(gps[20:22]), float(gps[23:27]), gps[28:29]))
-          lon = str(CoordinateToDouble(float(gps[30:33]), float(gps[33:35]), float(gps[36:40]), gps[41:42]))
-		  save_data(lat,lon,read_temp())
-       else:
-          lat = "NULL"
-          lon = "NULL"
+    if gps[1:6] == "GPGSA":
+        fix = int(gps[9:10])
+    if gps[1 : 6] == "GPGGA":
+        temp = str(read_temp())
+        if fix > 1:
+            lat = str(CoordinateToDouble(float(gps[18:20]), float(gps[20:22]), float(gps[23:27]), gps[28:29]))
+            lon = str(CoordinateToDouble(float(gps[30:33]), float(gps[33:35]), float(gps[36:40]), gps[41:42]))
+    	#  save_data(lat,lon,read_temp())
+        else:
+            lat = "NULL"
+            lon = "NULL"
 		  
-	print "Latitude: " + lat + " | Longitude: " + lng + " | Temperature: " + temp
-
+	print "Latitude: " + lat + " | Longitude: " + lon + " | Temperature: " + temp
