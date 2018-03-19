9f4c24d681b607efcc817522318bb20a07551a3b
DESKTOP-8A2NBQ9\Rav <ravneetsngh6@gmail.com>
2018-03-19 14:18:06 -0400

    Motor integration removed

--- tripV3.py
+++ tripV3.py
@@ -5,64 +5,10 @@ import time
 from time import *
 import serial
 import urllib2, urllib
-import RPi.GPIO as GPIO
-import time
-
-enable_pin = 25
-coil_A_1_pin = 20
-coil_A_2_pin = 21
-coil_B_1_pin = 19
-coil_B_2_pin = 26
-
-GPIO.setup(enable_pin, GPIO.OUT)
-GPIO.setup(coil_A_1_pin, GPIO.OUT)
-GPIO.setup(coil_A_2_pin, GPIO.OUT)
-GPIO.setup(coil_B_1_pin, GPIO.OUT)
-GPIO.setup(coil_B_2_pin, GPIO.OUT)
-
-GPIO.output(enable_pin, 1)
 
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
-
-def forward(delay, steps):
-	for i in range(0, steps):
-		setStep(1, 0, 1, 0)
-		time.sleep(delay)
-		setStep(0, 1, 1, 0)
-		time.sleep(delay)
-		setStep(0, 1, 0, 1)
-		time.sleep(delay)
-		setStep(1, 0, 0, 1)
-		time.sleep(delay)
-
-def backwards(delay, steps):
-	for i in range(0, steps):
-		setStep(1, 0, 0, 1)
-		time.sleep(delay)
-		setStep(0, 1, 0, 1)
-		time.sleep(delay)
-		setStep(0, 1, 1, 0)
-		time.sleep(delay)
-		setStep(1, 0, 1, 0)
-		time.sleep(delay)
-
-def setStep(w1, w2, w3, w4):
-	GPIO.output(coil_A_1_pin, w1)
-	GPIO.output(coil_A_2_pin, w2)
-	GPIO.output(coil_B_1_pin, w3)
-	GPIO.output(coil_B_2_pin, w4)
-  
-def startMotor(Status):
-	print Status
-	while True:
-		backwards(int(2) / 1000.0, int(128))
-		print "Motor opening"
-		time.sleep(15)
-		forward(int(2) / 1000.0, int(128))
-		print "Motor Closing"
-		time.sleep(15)
 
 def save_data(lat,lng,temp):
     #Save To Database
@@ -100,15 +46,11 @@ def read_temp():
 		temp_string = lines[1][equals_pos+2:]
 		temp_c = float(temp_string) / 1000.0
 		return temp_c
-		
-# Create two threads as follows
-try:
-   thread.start_new_thread( startMotor, ("Motor Started") )
-except:
-   print "Error: unable to start thread"
 
 ser = serial.Serial('/dev/ttyUSB0',4800,timeout = None)
+
 fix = 1
+
 x = 0
 while x == 0:
     gps = ser.readline()
