6c71835ec99c007ab38cdb0beabd56b71dcecf6f
DESKTOP-8A2NBQ9\Rav <ravneetsngh6@gmail.com>
2018-03-23 19:03:43 -0400

    temp difference

--- tripV3.py
+++ tripV3.py
@@ -110,23 +110,24 @@ def read_temp():
 ser = serial.Serial('/dev/ttyUSB0',4800,timeout = None)
 fix = 1
 x = 0
-
+oldTemp = 0;
 #set the scale
-rotateMotor(0, read_temp())
-
+rotateMotor(oldTemp, read_temp())
+oldTemp = read_temp()
 while x == 0:
     gps = ser.readline()
 
     if gps[1:6] == "GPGSA":
         fix = int(gps[9:10])
     if gps[1 : 6] == "GPGGA":
-        temp = str(read_temp())
+        newTemp = read_temp()
         if fix > 1:
             lat = str(CoordinateToDouble(float(gps[18:20]), float(gps[20:22]), float(gps[23:27]), gps[28:29]))
             lon = str(CoordinateToDouble(float(gps[30:33]), float(gps[33:35]), float(gps[36:40]), gps[41:42]))
-            save_data(lat,lon,temp)
+            save_data(lat,lon,str(newTemp))
         else:
             lat = "NULL"
             lon = "NULL"
 		  
-	print "Latitude: " + lat + " | Longitude: " + lon + " | Temperature: " + temp
+	
+	print "Latitude: " + lat + " | Longitude: " + lon + " | Temperature: " + str(newTemp) + " | Tempdiff: " + str(newTemp-oldTemp)
