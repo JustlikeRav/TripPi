81a2927dbb028ec440d25e00edf198d63c4defb7
DESKTOP-8A2NBQ9\Rav <ravneetsngh6@gmail.com>
2018-04-02 15:03:01 -0400

    update write

--- write_data.php
+++ write_data.php
@@ -11,7 +11,27 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO data (lat, lng, temp) VALUES ('".$_POST['lat']."', '".$_POST['lon']."', '".$_POST['temp']."')";
+$lat = 0;
+$lon = 0;
+
+if($_POST['lat'] == 'NULL' || $_POST['lon'] == 'NULL'){
+    $sql = "SELECT * FROM data ORDER BY data_id DESC LIMIT 1";
+	$result = mysqli_query($conn, $sql);
+
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$lat = $row["lat"];
+			$lon = $row["lng"];
+		}
+	} else {
+		echo "0 results";
+	}
+}else{
+	$lat = $_POST['lat'];
+	$lon = $_POST['lon'];
+}
+
+$sql = "INSERT INTO data (lat, lng, temp) VALUES ('".$lat."', '".$lon."', '".$_POST['temp']."')";
 echo $sql;
 
 if ($conn->query($sql) === TRUE) {
